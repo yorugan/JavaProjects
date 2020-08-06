8dcc2cd4393e5cd262dc9109b1a5dedb46f05e6f
yorugan <bsontec@gmail.com>
2020-08-06 21:11:45 +1200

    isPalindrome program which checks if the word is a palindrome.

---
+++ src/com/company/Palindrome.java
@@ -0,0 +1,40 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Palindrome
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Please enter a word to check for palindrome.");
+        Scanner input = new Scanner(System.in);
+        String word = input.nextLine();
+
+        if (isPalindrome(word))
+        {
+            System.out.printf("The word %s is a palindrome", word);
+        } else
+        {
+            System.out.printf("The word %s is not a palindrome", word);
+        }
+
+    }
+
+    public static boolean isPalindrome(String word)
+    {
+        String reversedString = "";
+        word.replaceAll("\\s+", "").toLowerCase();
+        for (int i = word.length() - 1; i >= 0; i--)
+        {
+            reversedString = reversedString + word.charAt(i);
+        }
+        if (word.equals(reversedString))
+        {
+            return true;
+        } else
+        {
+            return false;
+        }
+    }
+
+}
